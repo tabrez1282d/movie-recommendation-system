dd6b94084af531d5975047a77bce0c2c5eb0865a
Tabrez <tabrezdhoriwala@gmail.com>
2026-04-04 15:51:35 +0530

    Hide API key

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from thefuzz import process
 
 
 # Your TMDB API key
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 load_dotenv()
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
